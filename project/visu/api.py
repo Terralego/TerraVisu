57d4d02c8e811578f408918754ffa0d458d59801
J-E Castagnede <j.e.castagnede@gmail.com>
2022-11-23 12:29:07 +0100

    manage settings from django

--- project/visu/api.py
+++ project/visu/api.py
@@ -8,34 +8,34 @@ from project.visu import settings as app_settings
 class SettingsView(APIView):
     def get(self, request, *args, **kwargs):
         base_layers = MapBaseLayer.objects.all()
-        return Response({
-            "instance": {
-                "title": app_settings.INSTANCE_TITLE,
-                "logo": app_settings.INSTANCE_LOGO,
-                "loginUrl": reverse("login"),
-                "logoutUrl": reverse("logout"),
-            },
-            "map": {
-                "baseLayers": [
-                    {"label": layer.name,
-                     "slug": layer.slug,
-                     "url": f"{layer.url}"
-                     } for layer in base_layers
-                ],
-                "bounds": {
-                    "minLat": app_settings.MAP_BBOX_LAT_MIN,
-                    "minLon": app_settings.MAP_BBOX_LNG_MIN,
-                    "maxLat": app_settings.MAP_BBOX_LAT_MAX,
-                    "maxLon": app_settings.MAP_BBOX_LNG_MAX,
+        return Response(
+            {
+                "instance": {
+                    "title": app_settings.INSTANCE_TITLE,
+                    "logo": app_settings.INSTANCE_LOGO,
+                    "loginUrl": reverse("login"),
+                    "logoutUrl": reverse("logout"),
                 },
-                "zoom": {
-                    "min": app_settings.MAP_MIN_ZOOM,
-                    "max": app_settings.MAP_MAX_ZOOM,
+                "map": {
+                    "baseLayers": [
+                        {"label": layer.name, "slug": layer.slug, "url": f"{layer.url}"}
+                        for layer in base_layers
+                    ],
+                    "bounds": {
+                        "minLat": app_settings.MAP_BBOX_LAT_MIN,
+                        "minLon": app_settings.MAP_BBOX_LNG_MIN,
+                        "maxLat": app_settings.MAP_BBOX_LAT_MAX,
+                        "maxLon": app_settings.MAP_BBOX_LNG_MAX,
+                    },
+                    "zoom": {
+                        "min": app_settings.MAP_MIN_ZOOM,
+                        "max": app_settings.MAP_MAX_ZOOM,
+                    },
+                    "default": {
+                        "lat": app_settings.MAP_DEFAULT_LAT,
+                        "lng": app_settings.MAP_DEFAULT_LNG,
+                        "zoom": app_settings.MAP_DEFAULT_ZOOM,
+                    },
                 },
-                "default": {
-                    "lat": app_settings.MAP_DEFAULT_LAT,
-                    "lng": app_settings.MAP_DEFAULT_LNG,
-                    "zoom": app_settings.MAP_DEFAULT_ZOOM,
-                }
             }
-        })
+        )
