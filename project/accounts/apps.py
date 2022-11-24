8773ee1b0f03e6cb550a5deb3074b4e75e4299ff
J-E Castagnede <j.e.castagnede@gmail.com>
2022-11-24 10:55:00 +0100

    improve CI

--- project/accounts/apps.py
+++ project/accounts/apps.py
@@ -2,5 +2,6 @@ from django.apps import AppConfig as BaseAppConfig
 
 
 class AppConfig(BaseAppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
     name = "project.accounts"
     verbose_name = "Accounts"
