0a193c81a117d91f288bd92b6c71ca7eba33c1aa
J-E Castagnede <j.e.castagnede@gmail.com>
2022-11-25 15:34:34 +0100

    fix permissions

--- project/geosource/tests/helpers.py
+++ project/geosource/tests/helpers.py
@@ -1,13 +1,8 @@
-import os
-
 from django.conf import settings
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 
 def get_file(file_name):
-    try:
-        file_path = settings.BASE_DIR / "geosource" / "tests" / "data" / file_name
-        with open(file_path, "rb+") as f:
-            return SimpleUploadedFile(f.name, f.read())
-    except PermissionError:
-        raise Exception(f"Permission denied to read file {os.stat(file_path)}. Exiting : {os.path.exists(file_path)}")
+    file_path = settings.BASE_DIR / "geosource" / "tests" / "data" / file_name
+    with open(file_path, "rb+") as f:
+        return SimpleUploadedFile(f.name, f.read())
