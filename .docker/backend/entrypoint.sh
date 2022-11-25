f82efd2fa6356f79d6c8c77c7b4bf6b11837a3df
J-E Castagnede <j.e.castagnede@gmail.com>
2022-11-25 10:37:55 +0100

    fix permissions

--- .docker/backend/entrypoint.sh
+++ .docker/backend/entrypoint.sh
@@ -4,7 +4,7 @@
 . /opt/venv/bin/activate
 
 echo "Waiting for postgres..."
-while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
+while ! nc -z "$POSTGRES_HOST" "$POSTGRES_PORT"; do
     sleep 0.1
 done
 echo "PostgreSQL started"
