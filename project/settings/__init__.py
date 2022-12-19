fabd5b6f0b3ce5601efa2ae530f3db6a86f89e75
J-E Castagnede <j.e.castagnede@gmail.com>
2022-12-19 17:31:01 +0100

    add prop types

--- project/settings/__init__.py
+++ project/settings/__init__.py
@@ -23,12 +23,12 @@ PROJECT_DIR = BASE_DIR.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="please-override-me")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = config("ALLOWED_HOST", default=[], cast=Csv())
+ALLOWED_HOSTS = config("ALLOWED_HOST", default="", cast=Csv())
 
 
 # Application definition
