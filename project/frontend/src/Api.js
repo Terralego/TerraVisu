5a4e46571215808d84faf6d298179ba331295cd1
J-E Castagnede <j.e.castagnede@gmail.com>
2023-05-09 16:20:34 +0200

    clean front part

--- project/frontend/src/Api.js
+++
@@ -1,7 +0,0 @@
-export default function fetchSettings() {
-  return fetch('/api/settings/').then((response) => response.json());
-}
-
-export function fetchScenes() {
-  return fetch('/api/geolayer/scene/').then((response) => response.json());
-}
--- project/frontend/src/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import App from './components/App';
-
-const container = document.getElementById('app');
-const root = createRoot(container);
-const queryClient = new QueryClient();
-root.render(<QueryClientProvider client={queryClient} contextSharing><App /></QueryClientProvider>);
