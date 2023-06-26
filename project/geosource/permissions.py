8d230e2ba542f02b90f51e9d94f35c0a5e7d40e0
J-E Castagnede <j.e.castagnede@gmail.com>
2023-06-26 16:18:44 +0200

    fix source API permission

--- project/geosource/permissions.py
+++ project/geosource/permissions.py
@@ -3,4 +3,6 @@ from rest_framework import permissions
 
 class SourcePermission(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.has_terra_perm("can_manage_sources")
+        if request.user.is_authenticated:
+            return request.user.has_terra_perm("can_manage_sources")
+        return False
