e4e7c2b06ea37d2f0b29eb19ec203afd45905712
J-E Castagnede <j.e.castagnede@gmail.com>
2023-08-30 11:32:29 +0200

    fix task report state

--- project/geosource/mixins.py
+++ project/geosource/mixins.py
@@ -1,17 +1,11 @@
-from datetime import timedelta
-
 from celery import states
 from django.utils.timezone import now
 from rest_framework.exceptions import MethodNotAllowed
 
 from project.geosource.tasks import run_model_object_method
 
-from .app_settings import MAX_TASK_RUNTIME
-
 
 class CeleryCallMethodsMixin:
-    DONE_STATUSES = ("SUCCESS", "FAILURE", "NEED_SYNC", None)
-
     def update_status(self, task):
         self.task_id = task.task_id
         self.task_date = now()
@@ -20,13 +14,7 @@ class CeleryCallMethodsMixin:
     @property
     def can_sync(self):
         """Property containing a boolean that tell if the state allow to run a sync"""
-        status = self.get_status()
-
-        return status.get("state") in self.DONE_STATUSES or (
-            status.get("state") not in self.DONE_STATUSES
-            and self.task_date is not None
-            and self.task_date < now() - timedelta(hours=MAX_TASK_RUNTIME)
-        )
+        return self.status != self.STATUS.PENDING
 
     def run_async_method(
         self,
