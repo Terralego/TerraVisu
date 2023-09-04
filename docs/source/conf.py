b62f2b714024a8c0c381408cc8f9911394181945
J-E Castagnede <j.e.castagnede@gmail.com>
2023-09-04 10:28:13 +0200

    hotfix: rtd config file python version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,35 +3,37 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import datetime
+import os
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import os
 
 root = os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
-project = 'TerraVisu'
-copyright = '2022, Autonomens'
-author = 'Autonomens'
-release = open(os.path.join(root, 'project', 'VERSION')).read()
+project = "TerraVisu"
+copyright = f"2017 - {datetime.datetime.now().year}, Autonomens"
+author = "Autonomens"
+release = open(os.path.join(root, "project", "VERSION")).read()
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.todo',
-    'sphinx_rtd_theme',
+    "sphinx.ext.todo",
+    "sphinx_rtd_theme",
 ]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
 html_logo = "_static/logo.webp"
 html_theme_options = {
-    'logo_only': True,
+    "logo_only": True,
 }
