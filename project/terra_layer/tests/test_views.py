57b6b575eac1c2d310124f9d7eb3455c07e2a585
J-E Castagnede <j.e.castagnede@gmail.com>
2023-10-24 11:30:29 +0200

    fix WMTS group access (#406)

--- project/terra_layer/tests/test_views.py
+++ project/terra_layer/tests/test_views.py
@@ -669,7 +669,7 @@ class SceneTreeAPITestCase(APITestCase):
         group.authorized_layers.add(geo_layer)
 
         self.client.force_authenticate(self.user)
-        with self.assertNumQueries(43):
+        with self.assertNumQueries(42):
             self.client.get(reverse("layerview", args=[self.scene.slug]))
         with self.assertNumQueries(10):
             self.client.get(reverse("layerview", args=[self.scene.slug]))
@@ -678,7 +678,7 @@ class SceneTreeAPITestCase(APITestCase):
         layer.name = "new_name"
         layer.save()
 
-        with self.assertNumQueries(41):
+        with self.assertNumQueries(40):
             self.client.get(reverse("layerview", args=[self.scene.slug]))
 
     def test_cache_cleared_after_public_layer_update(self):
@@ -686,7 +686,7 @@ class SceneTreeAPITestCase(APITestCase):
         layer = Layer.objects.create(
             name="public_layer", source=source, group=self.layer_group
         )
-        with self.assertNumQueries(45):
+        with self.assertNumQueries(44):
             self.client.get(reverse("layerview", args=[self.scene.slug]))
 
         with self.assertNumQueries(9):
@@ -695,7 +695,7 @@ class SceneTreeAPITestCase(APITestCase):
         # updating layer to trigger cache reset
         layer.name = "new_name"
         layer.save()
-        with self.assertNumQueries(37):
+        with self.assertNumQueries(36):
             # still differences in original query number because callbacks auto create geostore layers and groups
             self.client.get(reverse("layerview", args=[self.scene.slug]))
 
