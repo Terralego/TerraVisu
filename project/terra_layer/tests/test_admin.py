8610baac5f80e0054142654ae52457c7c7556571
J-E Castagnede <j.e.castagnede@gmail.com>
2023-11-21 14:56:20 +0100

    fix tests

---
+++ project/terra_layer/tests/test_admin.py
@@ -0,0 +1,18 @@
+from django.test import TestCase
+
+from project.accounts.tests.factories import SuperUserFactory
+from project.terra_layer.tests.factories import LayerFactory
+
+
+class LayerAdminTesCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        LayerFactory.create_batch(10)
+        cls.user = SuperUserFactory.create()
+
+    def setUp(self):
+        self.client.force_login(self.user)
+
+    def test_list(self):
+        response = self.client.get("/admin/terra_layer/layer/")
+        self.assertEqual(response.status_code, 200)
