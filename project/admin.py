b0ef3f2a5d57c0565a1e791e2cd4a5682df3247b
J-E Castagnede <j.e.castagnede@gmail.com>
2024-11-25 12:28:25 +0100

    fix constance imort

--- project/admin.py
+++ project/admin.py
@@ -4,9 +4,10 @@ from django.utils.translation import gettext as _
 
 
 class ConfigSite(AdminSite):
-    site_header = _(f"{config.INSTANCE_TITLE}: {_('Configuration')}")
-    site_title = _(f"{config.INSTANCE_TITLE}: {_('Configuration')}")
-    index_title = _(f"Welcome to {config.INSTANCE_TITLE} configuration interface.")
-
+    def __init__(self, name="config_site"):
+        super().__init__(name)
+        self.site_header = _(f"{config.INSTANCE_TITLE}: {_('Configuration')}")
+        self.site_title = _(f"{config.INSTANCE_TITLE}: {_('Configuration')}")
+        self.index_title = _(f"Welcome to {config.INSTANCE_TITLE} configuration interface.")
 
 config_site = ConfigSite(name="config_site")
