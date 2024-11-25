ae603c9acce040b72ec368b15c5a11b155981660
J-E Castagnede <j.e.castagnede@gmail.com>
2024-11-25 12:53:32 +0100

    fix constance imort

--- project/admin.py
+++ project/admin.py
@@ -1,13 +1,11 @@
-from constance import config
 from django.contrib.admin import AdminSite
 from django.utils.translation import gettext as _
 
 
 class ConfigSite(AdminSite):
-    def __init__(self, name="config_site"):
-        super().__init__(name)
-        self.site_header = _(f"{config.INSTANCE_TITLE}: {_('Configuration')}")
-        self.site_title = _(f"{config.INSTANCE_TITLE}: {_('Configuration')}")
-        self.index_title = _(f"Welcome to {config.INSTANCE_TITLE} configuration interface.")
+    site_header = _("TerraVisu: Configuration")
+    site_title = _("TerraVisu: Configuration")
+    index_title = _("Welcome to TerraVisu configuration interface.")
+
 
 config_site = ConfigSite(name="config_site")
