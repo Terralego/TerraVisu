4b3e22b3b478277fe69fac64a382c5b27fd41fbb
J-E Castagnede <j.e.castagnede@gmail.com>
2025-03-20 16:54:18 +0100

    lint

--- project/settings/tests.py
+++ project/settings/tests.py
@@ -15,3 +15,14 @@ CONSTANCE_BACKEND = "constance.backends.memory.MemoryBackend"
 LANGUAGE_CODE = "en-us"
 OIDC_ENABLE_LOGIN = False
 TIME_ZONE = "UTC"
+
+
+class DisableMigrations:
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return None
+
+
+MIGRATION_MODULES = DisableMigrations()
