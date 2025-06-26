a8b0183ab7950eea4a60cfc89ca35f9d82c812e2
Célia Prat <celia.prat@makina-corpus.com>
2025-06-26 11:45:26 +0200

    Add unit test for report configs admin

--- project/terra_layer/tests/test_admin.py
+++ project/terra_layer/tests/test_admin.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 
 from project.accounts.tests.factories import SuperUserFactory
+from project.geosource.models import FieldTypes
+from project.terra_layer.models import ReportField
 from project.terra_layer.tests.factories import LayerFactory, ReportFactory
 
 
@@ -10,6 +12,14 @@ class LayerAdminTesCase(TestCase):
         LayerFactory.create_batch(10)
         cls.user = SuperUserFactory.create()
         cls.report = ReportFactory.create(user=cls.user)
+        field = cls.report.config.layer.source.fields.create(
+            name="test_field", label="test_label", data_type=FieldTypes.String.value
+        )
+        cls.report_field = ReportField.objects.create(
+            config=cls.report.config,
+            order=1,
+            field=field,
+        )
 
     def setUp(self):
         self.client.force_login(self.user)
@@ -24,4 +34,8 @@ class LayerAdminTesCase(TestCase):
 
     def test_reportconfig_list(self):
         response = self.client.get("/debug/terra_layer/reportconfig/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_reportfield_list(self):
+        response = self.client.get("/debug/terra_layer/reportfield/")
         self.assertEqual(response.status_code, 200)
