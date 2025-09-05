df9cd9ef029640caff2b13e2fbdb76ccbb701862
Célia Prat <celia.prat@makina-corpus.com>
2025-09-05 14:46:38 +0200

    Fix authent user to access files

--- project/views.py
+++ project/views.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
-from django.http import Http404, HttpResponse
+from django.http import Http404, HttpResponse, HttpResponseForbidden
 
 
 @login_required
@@ -8,6 +8,12 @@ def serve_private_files(request, path):
     file_path = settings.PRIVATE_MEDIA_ROOT / path
     if not file_path.exists():
         raise Http404()
+    if not (
+        request.user.is_report_manager
+        or request.user.is_declaration_manager
+        or request.user.is_superuser
+    ):
+        return HttpResponseForbidden()
     response = HttpResponse()
     response["X-Accel-Redirect"] = f"/private_files/media/{path}"
     response["Content-Type"] = ""  # Let nginx determine
