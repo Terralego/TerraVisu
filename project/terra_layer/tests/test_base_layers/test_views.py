4ffabbe24bec02a6c53d99505e80350a4c0b4691
Jean-Etienne Castagnede <j.e.castagnede@gmail.com>
2026-03-20 13:21:13 +0000

    fix tests

--- project/terra_layer/tests/test_base_layers/test_views.py
+++ project/terra_layer/tests/test_base_layers/test_views.py
@@ -29,7 +29,7 @@ class MapBaseLayerViewsSetTesCase(APITestCase):
         self.assertTrue(len(data["results"]) > 0)
 
     def test_detail_raster(self):
-        pk = MapBaseLayer.objects.get(slug="osm").pk
+        pk = MapBaseLayer.objects.get(name="OSM").pk
         response = self.client.get(reverse("baselayer-detail", args=(pk,)))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.json()
@@ -52,7 +52,7 @@ class MapBaseLayerViewsSetTesCase(APITestCase):
         )
 
     def test_tilejson_in_list(self):
-        pk = MapBaseLayer.objects.get(slug="osm").pk
+        pk = MapBaseLayer.objects.get(name="OSM").pk
         response = self.client.get(reverse("baselayer-detail", args=(pk,)))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.json()
